3973acb89a5b97f6fca4cc58e3412f3ae56034f5
Eduard Haris <eduard.haris@esolutions.ro>
2019-07-27 09:43:37 +0300

    am adaugat spring

---
+++ src/main/java/ro/esolutions/neversea/NeverseaApplication.java
@@ -0,0 +1,13 @@
+package ro.esolutions.neversea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverseaApplication.class, args);
+	}
+
+}
